598c2f3ab0d05d5fc9521bdd534fc2b15003132f
L Daniel Swakman <d.swakman@gmail.com>
2018-06-21 17:41:52 +0200

    base layout

---
+++ footer.php
@@ -0,0 +1,27 @@
+<?
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Selbst
+ * @since 1.0
+ * @version 1.0
+ */
+?>
+
+				</div><!-- #content -->
+
+				<footer id="colophon" class="site-footer" role="contentinfo">
+				</footer>
+
+			</div><!-- .site-content-contain -->
+		</div><!-- #page -->
+
+	<? wp_footer() ?>
+
+	</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,33 @@
+<?
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Selbst
+ * @since 1.0
+ * @version 1.0
+ */
+
+?><!DOCTYPE html>
+<html <? language_attributes(); ?> class="no-js no-svg">
+<head>
+<meta charset="<? bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link href="<?= get_template_directory_uri() ?>/style.css" rel="stylesheet" type="text/css" />
+
+<? wp_head(); ?>
+</head>
+
+<body <? body_class(); ?>>
+<div id="page">
+
+	<header id="masthead" class="site-header" role="banner">
+
+	</header>
+
+	<div class="site-content-contain">
+		<div id="content" class="site-content">
