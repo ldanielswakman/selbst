1fbcba331031dd5ea7593c754773a9fc9608112b
L Daniel Swakman <d.swakman@gmail.com>
2018-07-10 12:34:25 +0200

    added Benchmark newsletter signup

--- partials/newsletter-box.php
+++ partials/newsletter-box.php
@@ -2,9 +2,13 @@
 
 	<h3>Not ready to join yet, but want to stay in the loop?</h3>
 
-	<form>
+	<!-- BEGIN: Benchmark Email Signup Form Code -->
+	<script type="text/javascript" id="lbscript1072544" src="https://www.benchmarkemail.com/de/code/lbformnew.js?mFcQnoBFKMSnVX75qOf7CKjgVtVj7bhtT48Tk2j9HrGWc%252BjqsK41lA%253D%253D"></script><noscript>Please enable JavaScript <br /></noscript>
+	<!-- END: Benchmark Email Signup Form Code -->
+	
+	<!-- <form>
 		<input type="text" />
-	</form>
+	</form> -->
 
 	<p>Leave your email here, and we will occasionally send you an update to your inbox, informing you on what is going on with SELBST. We won’t share your data with other parties.</p>
 
